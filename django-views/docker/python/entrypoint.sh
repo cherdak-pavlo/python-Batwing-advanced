b2d6aafbbcaae08c84b3dd12caef35f531ef443c
cherdak-pavlo <75758329+cherdak-pavlo@users.noreply.github.com>
2022-08-23 02:11:38 +0300

    Delete django-views directory
    Fix some problems with git ignore

--- django-views/docker/python/entrypoint.sh
+++
@@ -1,1 +0,0 @@
-/django_project/docker/python/wait-for-it db:3306 -- python /django_project/manage.py migrate
--- django-views/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, sign_up, sign_in, logout_view
-
-urlpatterns = [
-    path("", home, name="home"),
-    path("sign-up", sign_up, name="sign-up"),
-    path("sign-in", sign_in, name="sign-in"),
-    path("logout", logout_view, name="logout")
-]
