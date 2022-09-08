afc25a771b90996bddd73ff94b66fd5efd8a1f3d
Pavlo Cherdak <killcrop123@gmail.com>
2022-09-08 14:27:37 +0300

    Complete HW #32

--- django_advance_practice/docker/python/entrypoint.sh
+++ django_advance_practice/docker/python/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 /django_project/docker/python/wait-for-it.sh db:3306 -- python /django_project/manage.py migrate
+/django_project/docker/python/wait-for-it.sh db:3306 -- python /django_project/manage.py loaddata seed/*
+
 gunicorn django_project.wsgi:application --bind 0.0.0.0:8000
