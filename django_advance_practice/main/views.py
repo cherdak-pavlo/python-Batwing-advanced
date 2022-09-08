7b746c76ca2ba5a0ab94dd3ffe4afa7b914b5ffc
Pavlo Cherdak <killcrop123@gmail.com>
2022-09-08 14:02:13 +0300

    complete hw #31

---
+++ django_advance_practice/main/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import MenuItem
+from products.models import Product, Order, OrderItem
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
+from .forms import LoginForm, RegisterForm
+
+
+def home(request):
+    menu_items = MenuItem.objects.all()
+    products = Product.objects.filter(display_on_main_page=True, approved=True).order_by("-id")
+    return render(request, 'main/index.html', {
+        "menu_items": menu_items,
+        "products": products
+    })
+
+
+# def sign_up(request):
+#     if request.method == "POST":
+#         user = User()
+#         user.username = request.POST.get("username")
+#         user.email = request.POST.get("email")
+#         user.set_password(request.POST.get("password"))
+#         user.first_name = request.POST.get("first_name")
+#         user.last_name = request.POST.get("last_name")
+#         user.is_superuser = False
+#         user.is_staff = False
+#         user.is_active = True
+#         user.save()
+#         login(request, user)
+#         return redirect("/")
+#     else:
+#         return render(request, "main/sign-up.html", {})
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get("password"))
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+        else:
+            return render(request, "main/sign-up.html", {"form": form})
+    else:
+        return render(request, "main/sign-up.html", {})
+
+
+# def sign_in(request):
+#     if request.method == "POST":
+#         user = authenticate(request, username=request.POST.get("username"), password=request.POST.get("password"))
+#         if user:
+#             login(request, user)
+#         return redirect("/")
+#     else:
+#         return render(request, "main/sign-in.html", {})
+
+
+def sign_in(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+        return render(request, "main/sign-in.html", {"form": form})
+    else:
+        return render(request, "main/sign-in.html", {})
+
+
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect("/")
+
+
+def add_to_cart(request, product_id):
+    if Product.objects.get(id=product_id):
+        if product_id in request.session.get("products", []):
+            return redirect("/")
+        if request.session.get("products", False):
+            request.session["products"].append(product_id)
+            request.session.modified = True
+        else:
+            request.session["products"] = []
+            request.session["products"].append(product_id)
+
+        total_price = 0
+        for products_id in request.session.get("products", []):
+            product_price = Product.objects.get(id=products_id).price
+            total_price += product_price
+        request.session["cart_total_price"] = total_price
+
+    else:
+        raise Http404()
+    return redirect("/")
+
+
+def cart(request):
+    if request.session.get("products", False):
+        products = Product.objects.filter(id__in=request.session.get("products"))
+        return render(request, "main/cart.html",
+                      {"products": products, "total_price": request.session.get("cart_total_price", 0)})
+    else:
+        return render(request, "main/cart.html", {})
+
+
+def remove_from_cart(request, product_id):
+    if request.session.get("products", False):
+        for i in range(len(request.session.get("products"))):
+            if request.session.get("products")[i] == product_id:
+                del request.session.get("products")[i]
+                request.session.modified = True
+
+                product = Product.objects.get(id=product_id)
+                request.session["cart_total_price"] -= product.price
+                break
+    return redirect("/cart")
+
+
+def order(request):
+    if request.session.get("products", False):
+        if request.user.is_authenticated:
+            if request.method == "GET":
+                return render(request, "main/order.html", {"total_price": request.session.get("cart_total_price", 0)})
+            else:
+                order = Order()
+                order.user = request.user
+                order.message = request.POST.get("description")
+                order.address = request.POST.get("address")
+                order.name = request.POST.get("name")
+                order.email = request.POST.get("email")
+                order.total_price = request.session.get("cart_total_price")
+                order.save()
+                for product_id in request.session.get("products", []):
+                    order_item = OrderItem()
+                    order_item.order = order
+                    order_item.product_id = product_id
+                    order_item.save()
+                request.session["products"] = []
+                request.session["cart_total_price"] = 0
+                return redirect("/")
---
+++ django_advance_practice/products/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Category
+from .forms import ProductForm
+from django.http import Http404
+
+
+def add_product(request):
+    if request.user.is_authenticated:
+        categories = Category.objects.order_by("-id")
+        if request.method == "GET":
+            form = ProductForm(initial={"user": request.user})
+            return render(request, "products/add.html", {"form": form, "categories": categories})
+        else:
+            form = ProductForm(request.POST)
+            print(request.POST)
+            if form.is_valid():
+                product = form.save(user=request.user)
+                if request.POST.getlist("categories", False):
+                    for category_id in request.POST.getlist("categories"):
+                        category = Category.objects.get(id=category_id)
+                        category.products.add(product)
+                return redirect("/")
+            else:
+                return render(request, "products/add.html", {"form": form, "categories": categories})
+    else:
+        return redirect("/")
+
+
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, "products/details.html",
+                  {"product": product, "product_session": request.session.get("products", False)})
+
+
+def add_category(request):
+    if request.user.is_authenticated and request.user.is_staff:
+        if request.method == "POST":
+            if request.POST.get("title"):
+                category = Category()
+                category.user = request.user
+                category.title = request.POST.get("title")
+                category.slug = request.POST.get("slug")
+                if request.POST.get("parent_category", False):
+                    category.parent_id = int(request.POST.get("parent_category"))
+                category.save()
+                return redirect("/")
+        else:
+            categories = Category.objects.order_by("-id")
+            return render(request, "products/category/add.html", {"categories": categories})
+
+
+def category_page(request, slug):
+    try:
+        category = Category.objects.get(slug=slug)
+    except Category.DoesNotExist:
+        raise Http404()
+    return render(request, "products/category_products.html", {"products": category.products.all})
+
+
+def update_product(request, id):
+    if request.user.is_authenticated:
+        product = Product.objects.get(id=id)
+        if product.user_id == request.user.id:
+            if request.method == "GET":
+                categories = Category.objects.order_by("-id")
+                return render(request, "products/update.html", {"product": product, "categories": categories})
+            else:
+                product.title = request.POST.get("title", '')
+                product.description = request.POST.get("description", '')
+                product.save()
+                if request.POST.getlist("categories", False):
+                    for category_id in request.POST.getlist("categories"):
+                        category = Category.objects.get(id=category_id)
+                        category.products.add(product)
+                return redirect("/")
+        else:
+            return redirect("/")
